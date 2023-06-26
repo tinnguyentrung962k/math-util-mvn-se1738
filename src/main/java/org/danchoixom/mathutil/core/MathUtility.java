8b0773ca444df38f17a84513c211b2145a5d1bf5
tinnguyentrung962k <trungtin9620@gmail.com>
2023-06-26 09:07:37 +0700

    update getf() to use recursion

--- src/main/java/org/danchoixom/mathutil/core/MathUtility.java
+++ src/main/java/org/danchoixom/mathutil/core/MathUtility.java
@@ -17,19 +17,26 @@ public class MathUtility {
     //0! = 1! = 1
     //21! LA KIEU LONG KO CHUA NOI 
     // GAI RANG BUOC VAO  CHO METHOD/HAM
+//    public static long getFactorial(int n){
+//        long product = 1; // tich ban dau bang 1
+//        if (n==0 || n==1){
+//            return 1;
+//        }
+//        if(n<0 || n>20){
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz.");
+//        }
+//        for (int i = 2; i <= n; i++) {
+//            product *= i; 
+//            
+//        }
+//        return product;
+//    }
     public static long getFactorial(int n){
-        long product = 1; // tich ban dau bang 1
-        if (n==0 || n==1){
-            return 1;
-        }
-        if(n<0 || n>20){
+        if (n<0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz.");
-        }
-        for (int i = 2; i <= n; i++) {
-            product *= i; 
-            
-        }
-        return product;
+        if (n==0 || n==1)
+            return 1;
+        return n * getFactorial(n-1);
     }
     //return som de ham khong bao loi
 }
